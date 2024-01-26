71e3cb817516885757ebdaa7b4273e6240d187e8
Federico Vazzoler <federico.vazzoler@cern.ch>
2024-01-26 16:19:54 +0100

    Add pytho scripts folder for reference

---
+++ python/__init__.py
@@ -0,0 +1,3 @@
+#Automatically created by SCRAM
+import os
+__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/Configuration/smp-22-010-qed-fsr-cmssw/',1)[0])+'/cfipython/slc7_amd64_gcc700/Configuration/smp-22-010-qed-fsr-cmssw')
