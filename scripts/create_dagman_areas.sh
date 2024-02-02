a051b38acab0a3431d8d62039bd02a5c4ecbbffb
Federico Vazzoler <federico.vazzoler@cern.ch>
2024-02-02 09:42:03 +0100

    Add muons

--- scripts/create_dagman_areas.sh
+++ scripts/create_dagman_areas.sh
@@ -25,3 +25,29 @@ BASE_DIR="/nfs/dust/cms/user/vazzolef/smp-22-010/qed-fsr-CMSSW/CMSSW_10_6_32_pat
                                 --cmsrun_config ${BASE_DIR}/configs/customLHE__Pythia8_no_MPI_PythiaQED_alphaEMorder_0__NanoGEN.py \
                                 --stageout_dir davs://eoscms.cern.ch:443/eos/cms/store/group/phys_smp/ec/sin2O/FSR_studies/ZToEE/FSR_PYTHIA_alphaEMorder_0 \
                                 --submit_job
+
+../scripts/create_condor_dag.py --dagman_area ${BASE_DIR}/condor_area/ZToMuMu__customLHE__Pythia8_no_MPI__NanoGEN \
+                                --input_lhe_folder /pnfs/desy.de/cms/tier2/store/user/fvazzole/powheg/crab_ZToMuMu_NLOQCD_LOEW__1_1000/ \
+                                --cmsrun_config ${BASE_DIR}/configs/customLHE__Pythia8_no_MPI__NanoGEN.py \
+                                --stageout_dir davs://eoscms.cern.ch:443/eos/cms/store/group/phys_smp/ec/sin2O/FSR_studies/ZToMuMu/FSR_OFF \
+                                --submit_job
+
+../scripts/create_condor_dag.py --dagman_area ${BASE_DIR}/condor_area/ZToMuMu__customLHE__Pythia8_no_MPI_Photos__NanoGEN \
+                                --input_lhe_folder /pnfs/desy.de/cms/tier2/store/user/fvazzole/powheg/crab_ZToMuMu_NLOQCD_LOEW__1_1000/ \
+                                --cmsrun_config ${BASE_DIR}/configs/customLHE__Pythia8_no_MPI_Photos__NanoGEN.py \
+                                --stageout_dir davs://eoscms.cern.ch:443/eos/cms/store/group/phys_smp/ec/sin2O/FSR_studies/ZToMuMu/PHOTOS_FSR \
+                                --submit_job
+
+
+../scripts/create_condor_dag.py --dagman_area ${BASE_DIR}/condor_area/ZToMuMu__customLHE__Pythia8_no_MPI_Photos_MEC_off__NanoGEN \
+                                --input_lhe_folder /pnfs/desy.de/cms/tier2/store/user/fvazzole/powheg/crab_ZToMuMu_NLOQCD_LOEW__1_1000/ \
+                                --cmsrun_config ${BASE_DIR}/configs/customLHE__Pythia8_no_MPI_Photos_MEC_off__NanoGEN.py \
+                                --stageout_dir davs://eoscms.cern.ch:443/eos/cms/store/group/phys_smp/ec/sin2O/FSR_studies/ZToMuMu/FSR_PHOTOS_MEC_OFF \
+                                --submit_job
+
+
+../scripts/create_condor_dag.py --dagman_area ${BASE_DIR}/condor_area/ZToMuMu__customLHE__Pythia8_no_MPI_PythiaQED_alphaEMorder_0__NanoGEN \
+                                --input_lhe_folder /pnfs/desy.de/cms/tier2/store/user/fvazzole/powheg/crab_ZToMuMu_NLOQCD_LOEW__1_1000/ \
+                                --cmsrun_config ${BASE_DIR}/configs/customLHE__Pythia8_no_MPI_PythiaQED_alphaEMorder_0__NanoGEN.py \
+                                --stageout_dir davs://eoscms.cern.ch:443/eos/cms/store/group/phys_smp/ec/sin2O/FSR_studies/ZToMuMu/FSR_PYTHIA_alphaEMorder_0 \
+                                --submit_job
